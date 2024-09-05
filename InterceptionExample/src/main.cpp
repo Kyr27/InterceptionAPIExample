0f3e836c8d68956da5fdb1f08841bfeb8bc77e53
Kyr27 <001eff.dks@gmail.com>
2024-09-06 00:01:30 +0200

    Add project files.

---
+++ InterceptionExample/src/main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <thread>
+#include <atomic>
+#include "interception.h"
+
+// The libs are for both debug and release configurations, because they contain debug symbols within them.
+#pragma comment(lib, "interception.lib")
+
+InterceptionContext context{ NULL };
+
+void send_key(InterceptionContext context, InterceptionDevice device, unsigned short scanCode, bool keyUp = false)
+{
+	InterceptionKeyStroke stroke;
+	stroke.code = scanCode;
+	stroke.state = keyUp ? INTERCEPTION_KEY_UP : INTERCEPTION_KEY_DOWN;
+
+	interception_send(context, device, (InterceptionStroke*)&stroke, 1);
+}
+
+void send_key_loop(InterceptionContext context, InterceptionDevice device, UINT scanCode, std::atomic<bool>& running) {
+	while (running) {
+		send_key(context, device, scanCode);
+		Sleep(200);
+		send_key(context, device, scanCode, true);
+		Sleep(50);
+	}
+}
+
+void process_key_event(InterceptionKeyStroke& stroke) {
+	std::cout << "Key event: code = " << stroke.code
+		<< ", state = " << stroke.state << std::endl;
+}
+
+BOOL WINAPI ConsoleHandler(DWORD dwCtrlType) {
+	switch (dwCtrlType) {
+	case CTRL_C_EVENT:
+	case CTRL_BREAK_EVENT:
+	case CTRL_CLOSE_EVENT:
+	case CTRL_LOGOFF_EVENT:
+	case CTRL_SHUTDOWN_EVENT:
+		if (context != NULL) {
+			// Destroy the context if the application is closed via any of the above cases
+			interception_destroy_context(context);
+			std::cout << "Context destroyed\n";
+		}
+		return TRUE;
+	default:
+		return FALSE;
+	}
+}
+
+int main()
+{
+	// Used to control the multi-threaded loop
+	std::atomic<bool> running{ true };
+
+
+	// Set Console Control Handler, so we can handle all console closing actions
+
+	if (!SetConsoleCtrlHandler(ConsoleHandler, true))
+	{
+		std::cerr << "Failed to set console control handler\n";
+		return EXIT_FAILURE;
+	}
+
+
+	// Initialize interception context
+
+	context = interception_create_context();
+	std::cout << "Context Set\n";
+
+
+	// Set the event filtering to keyboard, so we listen in on keyboard events
+
+	interception_set_filter(context, interception_is_keyboard, INTERCEPTION_FILTER_KEY_ALL);
+	std::cout << "Filter set\n";
+
+
+	// Wait for input event from any keyboard device and generate a handle to that device
+
+	InterceptionDevice device = interception_wait(context);
+
+
+	// Convert Virtual Keycodes into ScanCodes that interception can read
+
+	UINT scanCodeA = MapVirtualKey(0x41, MAPVK_VK_TO_VSC);
+	UINT scanCodeEnd = MapVirtualKey(VK_END, MAPVK_VK_TO_VSC);
+
+
+	// Run the key loop in another thread, so as to not interrupt the thread that is checking for scanCodeEnd
+
+	std::thread keyThread(send_key_loop, context, device, scanCodeA, std::ref(running)); // The std::ref() function ensures that running is passed by reference to the new thread, as std::atomic<bool> cannot be copied. Without std::ref(), std::thread will attempt to copy the arguments leading to errors.
+
+
+	// Wait for End key to be pressed before terminating (non keyboard blocking)
+
+	InterceptionStroke stroke;
+	while (running) {
+		int receivedKeys = interception_receive(context, device, &stroke, 1);
+
+		if (receivedKeys > 0) {
+			InterceptionKeyStroke& keyStroke = *(InterceptionKeyStroke*)&stroke;
+
+			std::cout << "Key pressed: " << keyStroke.code << std::endl;
+
+			// Terminate the loop and exit the application if the user pressed the End button
+			if (keyStroke.code == scanCodeEnd) {
+				running = false;
+				break;
+			}
+
+			// could even make it so that every key i press is modified into the A key
+			// keyStroke.code = scanCodeA
+
+			// Continue sending the stroke to the OS
+			interception_send(context, device, &stroke, 1);
+		}
+
+		Sleep(10); // Prevent high CPU usage
+	}
+
+	// Inform the user that application is exiting
+
+	std::cout << "Exiting application...\n";
+
+
+	// Join the two threads back into one
+
+	keyThread.join();
+
+
+	// Destroy the context
+
+	if (context != NULL) interception_destroy_context(context);
+
+
+	// Wait for user to confirm
+
+	std::cout << "Press any key to continue...\n";
+	static_cast<void>(_getch());  // cast to void to tell the compiler to shut up
+
+	return EXIT_SUCCESS;
+}
---
+++ driver-installation/readme.txt
@@ -0,0 +1,2 @@
+open cmd, navigate to this directory and run "install-interception /install"
+after a sucessfull installation you will need to reboot
